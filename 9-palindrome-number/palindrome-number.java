8bdb37ee20e3d0241103de4a47a3158072288c16
SRI RAKSHA <23cs109@drngpit.ac.in>
2025-07-28 19:45:34 +0530

    Time: 5 ms (66.16%) | Memory: 44 MB (76.08%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+          int r=0;
+          int s=x; 
+          while(x>0){
+            r=r*10+x%10;
+            x=x/10;
+          }
+          if(s==r){
+            return true;
+          }
+          else{
+            return false;
+          }
+    }
+}
