7f1e827c65764ac9206488ed13c1e624f04ab947
SRI RAKSHA <23cs109@drngpit.ac.in>
2025-07-31 21:18:40 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (74.63%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int cur=0;
+        int max=0;
+        for(int i=0;i<gain.length;i++){
+            cur=cur+gain[i];
+            max=Math.max(cur,max);
+        }
+        return max;
+        
+    }
+}
