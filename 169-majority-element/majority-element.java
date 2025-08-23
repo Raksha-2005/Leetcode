430e962697b4e4421dffd0295e1272ae8d4d7213
SRI RAKSHA <23cs109@drngpit.ac.in>
2025-08-23 19:20:19 +0530

    Time: 2 ms (73.84%) | Memory: 53.3 MB (38.18%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count=0;
+        int a=0;
+        for(int i=0;i<nums.length;i++){
+            if(count==0){
+                a=nums[i];
+            }
+            if(nums[i]==a){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return a;
+        
+    }
+}
